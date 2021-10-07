0ec00a2a394d5555680cc1e8663b47d38c970e77
nearbystays <86144003+nearbystays@users.noreply.github.com>
2021-10-06 20:14:28 -0700

    Delete Php directory

--- Php/assoc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
-
-$arr = json_decode($jsonobj, true);
-
-echo $arr["Peter"];
-echo $arr["Ben"];
-echo $arr["Joe"];
-?>
--- Php/file.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo readfile("webdictionary.txt");
-?>
-<?php
-$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("webdictionary.txt"));
-fclose($myfile);
-?>
