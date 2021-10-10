e0639e82760e2285bc481c9369ca1638834f6af0
nearbystays <nearbystays.io@gmail.com>
2021-10-09 19:56:22 -0700

    Maintaince

--- search/index.js
+++ search/index.js
@@ -1,10 +1,3 @@
-// This example uses the autocomplete feature of the Google Places API.
-// It allows the user to find all hotels in a given place, within a given
-// country. It then displays markers for all the hotels returned,
-// with on-click details for each hotel.
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 let map;
 let places;
 let infoWindow;
