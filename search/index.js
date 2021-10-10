0746ad1f749e0dc2b9bc8c897809b39f01a1f350
nearbystays <nearbystays.io@gmail.com>
2021-10-10 02:54:22 -0700

    Maintaince

--- search/index.js
+++ search/index.js
@@ -25,7 +25,7 @@ const countries = {
 
 function initMap() {
   console.log("Nearby Stays: Luxury Stays Anywhere!");
-  map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map($("#map"), {
     zoom: countries["us"].zoom,
     center: countries["us"].center,
     mapTypeControl: false,
@@ -35,14 +35,13 @@ function initMap() {
   });
   // console.log(map);
   infoWindow = new google.maps.InfoWindow({
-    content: document.getElementById("info-content"),
+    content: $("#info-content"),
   });
   // Create the autocomplete object and associate it with the UI input control.
   // Restrict the search to the default country, and to place type "cities".
   autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById("autocomplete"),
+    $("#autocomplete"),
     {
-      types: ["(cities)"],
       componentRestrictions: countryRestrict,
     }
   );
@@ -64,7 +63,7 @@ function onPlaceChanged() {
     map.setZoom(15);
     search();
   } else {
-    document.getElementById("autocomplete").placeholder = "Enter a city";
+    $("#autocomplete").placeholder = "Enter a city";
   }
 }
 
@@ -116,7 +115,7 @@ function clearMarkers() {
 // Set the country restriction based on user input.
 // Also center and zoom the map on the given country.
 function setAutocompleteCountry() {
-  const country = document.getElementById("country").value;
+  const country = $("#country").value;
 
   if (country == "all") {
     autocomplete.setComponentRestrictions({ country: [] });
@@ -139,7 +138,7 @@ function dropMarker(i) {
 }
 
 function addResult(result, i) {
-  const results = document.getElementById("results");
+  const results = $("#results");
   const markerLetter = String.fromCharCode("A".charCodeAt(0) + (i % 26));
   const markerIcon = MARKER_PATH + markerLetter + ".png";
   const tr = document.createElement("tr");
@@ -167,7 +166,7 @@ function addResult(result, i) {
 }
 
 function clearResults() {
-  const results = document.getElementById("results");
+  const results = $("#results");
 
   while (results.childNodes[0]) {
     results.removeChild(results.childNodes[0]);
@@ -194,17 +193,17 @@ function showInfoWindow() {
 
 // Load the place information into the HTML elements used by the info window.
 function buildIWContent(place) {
-  document.getElementById("iw-icon").innerHTML =
+  $("#iw-icon").innerHTML =
     '<img class="hotelIcon" ' + 'src="' + place.icon + '"/>';
-  document.getElementById("iw-url").innerHTML =
+  $("#iw-url").innerHTML =
     '<b><a href="' + place.url + '">' + place.name + "</a></b>";
-  document.getElementById("iw-address").textContent = place.vicinity;
+  $("#iw-address").textContent = place.vicinity;
   if (place.formatted_phone_number) {
-    document.getElementById("iw-phone-row").style.display = "";
-    document.getElementById("iw-phone").textContent =
+    $("#iw-phone-row").style.display = "";
+    $("#iw-phone").textContent =
       place.formatted_phone_number;
   } else {
-    document.getElementById("iw-phone-row").style.display = "none";
+    $("#iw-phone-row").style.display = "none";
   }
 
   // Assign a five-star rating to the hotel, using a black star ('&#10029;')
@@ -214,17 +213,13 @@ function buildIWContent(place) {
     let ratingHtml = "";
 
     for (let i = 0; i < 5; i++) {
-      if (place.rating < i + 0.5) {
-        ratingHtml += "&#10025;";
-      } else {
-        ratingHtml += "&#10029;";
-      }
+      place.rating < i+.5 ? ratingHtml += "&#10025;" : ratingHtml += "&#10029;";
 
-      document.getElementById("iw-rating-row").style.display = "";
-      document.getElementById("iw-rating").innerHTML = ratingHtml;
+      $("#iw-rating-row").style.display = "";
+      $("#iw-rating").innerHTML = ratingHtml;
     }
   } else {
-    document.getElementById("iw-rating-row").style.display = "none";
+    $("#iw-rating-row").style.display = "none";
   }
 
   // The regexp isolates the first part of the URL (domain plus subdomain)
@@ -238,9 +233,9 @@ function buildIWContent(place) {
       fullUrl = website;
     }
 
-    document.getElementById("iw-website-row").style.display = "";
-    document.getElementById("iw-website").textContent = website;
+    $("#iw-website-row").style.display = "";
+    $("#iw-website").textContent = website;
   } else {
-    document.getElementById("iw-website-row").style.display = "none";
+    $("#iw-website-row").style.display = "none";
   }
 }
