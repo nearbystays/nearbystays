5629b950861f5a7f008306869953a0fb82e3e640
nearbystays <nearbystays.io@gmail.com>
2021-10-26 22:44:52 -0700

    Maintaince

---
+++ navigation/index.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function(event){
+  navigator.geolocation ?
+    navigator.geolocation.getCurrentPosition(showPosition) :
+    alert("Geolocation is not supported by current browser");
+  }
+});
+
+function showPosition(position) {
+  var userPosition = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  };
+}
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: userPosition,
+    zoom: 14,
+  });
+}
