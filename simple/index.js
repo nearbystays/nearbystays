0f69a36c70ed093355094cbb7371bfd10622810e
Jeremy Scott <81507007+thearhus@users.noreply.github.com>
2021-10-28 00:03:11 -0700

    Maintaince

--- simple/index.js
+++ simple/index.js
@@ -3,7 +3,7 @@ let local;
 
 window.addEventListener('DOMContentLoaded', () => {
   initMap();
-  // locator();
+  locator();
 });
 
 function locator() {
@@ -13,32 +13,20 @@ function locator() {
         lat: p.coords.latitude,
         lng: p.coords.longitude,
       };
-    // initMap(local);
-    return local
+    initMap(local);
     });
   }
 }
 
-document.
-
-// function initMap(usr) {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: usr,
-//     zoom: 14,
-//   });
-// }
-
-
-window['initMap'] = function(usr) {
-  console.log(locator());
+function initMap(usr) {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: locator(),
+    center: usr,
     zoom: 14,
   });
 }
 
 var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDLgwI8A-l0MY0LxZSdUcPJZgsFSYSKG78&callback=initMap';
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAAdMnbeugzorXUqv735wPfnaSTt4qFWvs&callback=initMap';
 script.async = true;
 
 document.head.appendChild(script)
