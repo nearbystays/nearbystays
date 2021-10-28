ebe5be7f447778779bd512f42c16f2227f1367d9
Jeremy Scott <81507007+thearhus@users.noreply.github.com>
2021-10-28 00:22:23 -0700

    Maintaince

--- simple/index.js
+++ simple/index.js
@@ -17,6 +17,10 @@ function locator() {
   }
 }
 
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDLgwI8A-l0MY0LxZSdUcPJZgsFSYSKG78&callback=initMap';
+script.async = true;
+
 function initMap() {
   debugger;
   map = new google.maps.Map(document.getElementById("map"), {
@@ -24,9 +28,5 @@ function initMap() {
     zoom: 14,
   });
 }
-
-var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDLgwI8A-l0MY0LxZSdUcPJZgsFSYSKG78&callback=initMap';
-script.async = true;
 
 document.head.appendChild(script)
