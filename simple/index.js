da4e5fe08a1e532a8dc6da7575c84fa53b1f7f2b
Jeremy Scott <81507007+thearhus@users.noreply.github.com>
2021-10-28 00:32:46 -0700

    Maintaince

--- simple/index.js
+++ simple/index.js
@@ -21,6 +21,8 @@ var script = document.createElement('script');
 script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDLgwI8A-l0MY0LxZSdUcPJZgsFSYSKG78&callback=initMap';
 script.async = true;
 
+document.head.appendChild(script)
+
 function initMap() {
   debugger;
   map = new google.maps.Map(document.getElementById("map"), {
@@ -28,5 +30,3 @@ function initMap() {
     zoom: 14,
   });
 }
-
-document.head.appendChild(script)
