da5007bb1b39f739edd6570ea709e2acf4c2fc27
nearbystays <nearbystays.io@gmail.com>
2021-11-03 00:01:18 -0700

    Maintaince

--- hotel/index.js
+++ hotel/index.js
@@ -45,13 +45,8 @@ function initMap(geography) {
   });
   // Create the autocomplete object and associate it with the UI input control.
   // Restrict the search to the default country, and to place type "cities".
-  autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById("autocomplete"),
-    {
-      // types: ["(regions)", "address", "establishment", "(citites)"],
-      // componentRestrictions: countryRestrict,
-    }
-  );
+  var auto = document.getElementById("autocomplete")
+  autocomplete = new google.maps.places.Autocomplete(auto);
   places = new google.maps.places.PlacesService(map);
   autocomplete.addListener("place_changed", onPlaceChanged);
   // document.getElementById("country").addEventListener("change", setAutocompleteCountry);
