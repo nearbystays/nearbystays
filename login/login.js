15685ed5d4a6015bd14eb9ea3080a820dfd1ab41
Jeremy Scott <81507007+thearhus@users.noreply.github.com>
2021-12-06 04:40:14 -0800

    Maintaince

--- login/login.js
+++ login/login.js
@@ -1,16 +1,15 @@
-        function handleCredentialResponse(response) {
-          console.log("Encoded JWT ID token: " + response.credential);
-        }
-        window.onload = function () {
-          google.accounts.id.initialize({
-            client_id: "YOUR_GOOGLE_CLIENT_ID",
-            callback: handleCredentialResponse
-          });
-          google.accounts.id.renderButton(
-            document.getElementById("buttonDiv"),
-            { theme: "outline", size: "large" }  // customization attributes
-          );
-          google.accounts.id.prompt(); // also display the One Tap dialog
-        }
-    </script>
-    <div id="buttonDiv"></div> 
+function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
+}
+
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "86822582342-ohkbhad3csa8tsg85h9cp37fiu628u97.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large" }  // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+}
